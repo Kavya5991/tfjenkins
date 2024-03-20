48be32af2c496f95874427c40308a48961167780
Kavya5991 <130562048+Kavya5991@users.noreply.github.com>
2024-03-20 01:49:17 -0400

    Update main.tf

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -65,14 +65,16 @@ resource "aws_launch_template" "launch_template" {
 
  user_data =  <<-EOF
               #!/bin/bash
-              # Install Jenkins
-              sudo apt-get update
-              sudo apt-get install -y default-jdk
-              wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-              sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-              sudo apt-get update
-              sudo apt-get install -y jenkins
-              EOF
+sudo su
+yum update -y  # updates the package list and upgrades installed packages on the system
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo  #downloads the Jenkins repository configuration file and saves it to /etc/yum.repos.d/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key  #imports the GPG key for the Jenkins repository. This key is used to verify the authenticity of the Jenkins packages
+yum upgrade -y #  upgrades packages again, which might be necessary to ensure that any new dependencies required by Jenkins are installed
+dnf install java-11-amazon-corretto -y  # installs Amazon Corretto 11, which is a required dependency for Jenkins.
+yum install jenkins -y  
+systemctl enable jenkins  #enables the Jenkins service to start automatically at boot time
+systemctl start jenkins                
+EOF
 }
 
 resource "aws_autoscaling_group" "auto_scaling_group" {
