344f5ecbc073a61a1468eb5d51e4f9207c79f945
Kavya5991 <130562048+Kavya5991@users.noreply.github.com>
2024-03-21 22:05:24 -0400

    Update main.tf

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -63,18 +63,7 @@ resource "aws_launch_template" "launch_template" {
     }
   }
 
- user_data =  <<-EOF
-              #!/bin/bash
-sudo su
-yum update -y  # updates the package list and upgrades installed packages on the system
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo  #downloads the Jenkins repository configuration file and saves it to /etc/yum.repos.d/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key  #imports the GPG key for the Jenkins repository. This key is used to verify the authenticity of the Jenkins packages
-yum upgrade -y #  upgrades packages again, which might be necessary to ensure that any new dependencies required by Jenkins are installed
-dnf install java-11-amazon-corretto -y  # installs Amazon Corretto 11, which is a required dependency for Jenkins.
-yum install jenkins -y  
-systemctl enable jenkins  #enables the Jenkins service to start automatically at boot time
-systemctl start jenkins                
-EOF
+ user_data = filebase64("https://raw.githubusercontent.com/Kavya5991/tfjenkins/main/jenkins_install.sh")
 }
 
 resource "aws_autoscaling_group" "auto_scaling_group" {
